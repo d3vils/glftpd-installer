8fb99605606569482fc48b71c3ce013f7b4bc98e
teqnodude <teqno@no1.mine.nu>
2019-09-12 11:53:32 +0200

    Forgot minor cosmetic change

--- packages/scripts/tur-predircheck_manager/tur-predircheck_manager.tcl
+++ packages/scripts/tur-predircheck_manager/tur-predircheck_manager.tcl
@@ -7,13 +7,6 @@
 # Change mainchania below to your irc channel. Users must be in that chan or #
 # they will be ignored. No capital letters in mainchan.                      #
 #                                                                            #
-# If using a /msg to the bot, the user must be in the mainchan.              #
-#                                                                            #
-# The o in the binds means that only ops can run it. By ops, I mean users    #
-# added to THIS bot with +o in the mainchan. Just being a @ dosnt help.      #
-# You can use any flag you want, just make sure the user is added in the bot #
-# with that flag, in the #mainchan.                                          #
-#                                                                            #
 # If you change irc trigger here then be sure it's the same in the script    #
 # itself under "irctrigger"                                                  #
 #                                                                            #
