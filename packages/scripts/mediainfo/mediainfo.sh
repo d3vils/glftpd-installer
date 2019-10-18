03bc55ace59d3577de0116d0df36681cbd6f19ef
teqnodude <teqno@no1.mines.nu>
2019-10-18 02:14:08 +0200

    Fix - Optimized Mediainfo and added the removal of tmp file since it could cause permission problems if run in shell and then in irc

--- packages/scripts/mediainfo/mediainfo.sh
+++ packages/scripts/mediainfo/mediainfo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VER=1.2
+VER=1.3
 #---------------------------------------------------------------#
 #                                                               #
 # Mediainfo by Teqno                                       	#
@@ -11,6 +11,7 @@ VER=1.2
 
 GLROOT=/glftpd
 TMP=$GLROOT/tmp
+TMPFILE=$TMP/mediainfo.txt
 INPUT=`echo "$@" | cut -d " " -f2`
 TV=`echo $INPUT | grep -o ".*.S[0-9][0-9]E[0-9][0-9].\|.*.E[0-9][0-9]."`
 MOVIE=`echo $INPUT | sed 's/[0-9][0-9][0-9][0-9]p//' | grep -o ".*[0-9][0-9][0-9][0-9]."`
@@ -44,18 +45,34 @@ else
 	esac
     else
 	case $INPUT in
-	    *.2160p.*)
-	    section=TV-2160
-	    release="$TV*2160p*"
-	    ;;
-	    *.1080p.*)
-	    section=TV-1080
-	    release="$TV*1080p*"
-    	    ;;
-	    *.720p.*)
-	    section=TV-720
-	    release="$TV*720p*"
-	    ;;
+            *.2160p.*)
+            section=TV-2160
+            release="$TV*2160p*"
+            ;;
+            *.DANiSH.1080p.*|*.DANISH.1080p.*|*.SWEDiSH.1080p.*|*.FINNISH.1080p.*)
+            section=TV-NORDIC
+            release="$TV*1080p*"
+            ;;
+            *.NORWEGiAN.1080p.*)
+            section=TV-NO
+            release="$TV*1080p*"
+            ;;
+            *.1080p.*)
+            section=TV-1080
+            release="$TV*1080p*"
+            ;;
+            *.DANiSH.720p.*|*.DANISH.720p.*|*.SWEDiSH.720p.*|*.FINNISH.720p.*)
+            section=TV-NORDIC
+            release="$TV*720p*"
+            ;;
+            *.NORWEGiAN.720p.*)
+            section=TV-NO
+            release="$TV*720p*"
+            ;;
+            *.720p.*)
+            section=TV-720
+            release="$TV*720p*"
+            ;;
 	    *)
             echo -e $HELP
 	    exit 0
@@ -64,36 +81,37 @@ else
     fi
     cd $GLROOT/bin
     if [ ! -d $TMP ]; then mkdir -m777 $GLROOT/tmp ; fi
-    for info in `find $GLROOT/site/$section -maxdepth 1 -type d -name "$release"`
+    for info in `ls $GLROOT/site/$section | grep -v ".*NUKED.*" | grep "$release"`
     do
-	for media in `find $info -name "*.rar" -not -path "*/Subs*"`
+	for media in `ls $GLROOT/site/$section/$info | grep ".*.rar"`
 	do
-            ./mediainfo-rar $media > $TMP/mediainfo.txt
-            release=`cat $TMP/mediainfo.txt | grep "^Filename" | cut -d ":" -f2 | sed -e "s|$GLROOT/site/$section/||" -e 's|/.*||' -e 's/ //'`
+            ./mediainfo-rar $media > $TMPFILE
+            release=`cat $TMPFILE | grep "^Filename" | cut -d ":" -f2 | sed -e "s|$GLROOT/site/$section/||" -e 's|/.*||' -e 's/ //'`
             echo -n "$release"
-            filesize=`cat $TMP/mediainfo.txt | grep "File size*" | grep "MiB\|GiB" | cut -d ":" -f2 | sed 's/ //'`
+            filesize=`cat $TMPFILE | grep "File size*" | grep "MiB\|GiB" | cut -d ":" -f2 | sed 's/ //'`
             echo -n " | $filesize"
-            duration=`cat $TMP/mediainfo.txt | sed -n '/General/,/Video/p' | grep "^Duration" | cut -d ":" -f2 | sed 's/ //'`
+            duration=`cat $TMPFILE | sed -n '/General/,/Video/p' | grep "^Duration" | cut -d ":" -f2 | sed 's/ //'`
             echo -n " | $duration"
-            obitrate=`cat $TMP/mediainfo.txt | sed -n '/General/,/Video/p' | grep -v "Overall bit rate mode" | grep "Overall bit rate" | cut -d ":" -f2 | sed 's/ //'`
+            obitrate=`cat $TMPFILE | sed -n '/General/,/Video/p' | grep -v "Overall bit rate mode" | grep "Overall bit rate" | cut -d ":" -f2 | sed 's/ //'`
             if [ "$obitrate" ]; then echo -n " | Overall: $obitrate" ; fi
-            vbitrate=`cat $TMP/mediainfo.txt | sed -n '/Video/,/Forced/p' | grep "^Bit rate  " | cut -d ":" -f2 | sed 's/ //'`
+            vbitrate=`cat $TMPFILE | sed -n '/Video/,/Forced/p' | grep "^Bit rate  " | cut -d ":" -f2 | sed 's/ //'`
             if [ "$vbitrate" ]; then echo -n " | Video: $vbitrate" ; fi
-            nbitrate=`cat $TMP/mediainfo.txt | sed -n '/Video/,/Forced/p' | grep "^Nominal bit rate  " | cut -d ":" -f2 | sed 's/ //'`
+            nbitrate=`cat $TMPFILE | sed -n '/Video/,/Forced/p' | grep "^Nominal bit rate  " | cut -d ":" -f2 | sed 's/ //'`
             if [ "$nbitrate" ]; then  echo -n " | Video Nominal: $nbitrate" ; fi
-            if [ -z "`cat $TMP/mediainfo.txt | sed -n '/Audio #1/,/Forced/p'`" ]; then audio="Audio" ;  else audio="Audio #1" ; fi
-            abitrate=`cat $TMP/mediainfo.txt | sed -n "/$audio/,/Forced/p" | grep "^Bit rate  " | cut -d ":" -f2 | sed 's/ //'`
+            if [ -z "`cat $TMPFILE | sed -n '/Audio #1/,/Forced/p'`" ]; then audio="Audio" ;  else audio="Audio #1" ; fi
+            abitrate=`cat $TMPFILE | sed -n "/$audio/,/Forced/p" | grep "^Bit rate  " | cut -d ":" -f2 | sed 's/ //'`
             if [ "$abitrate" ]; then echo -n " | Audio: $abitrate" ; fi
-            mabitrate=`cat $TMP/mediainfo.txt | sed -n "/$audio/,/Forced/p" | grep "^Maximum bit rate  " | cut -d ":" -f2 | sed 's/ //'`
+            mabitrate=`cat $TMPFILE | sed -n "/$audio/,/Forced/p" | grep "^Maximum bit rate  " | cut -d ":" -f2 | sed 's/ //'`
             if [ "$mabitrate" ]; then echo -n " | Max Audio: $mabitrate" ; fi
-            format=`cat $TMP/mediainfo.txt | sed -n "/$audio/,/Forced/p" | grep "^Format  " | cut -d ":" -f2 | sed -e 's/ //' -e 's/UTF\-8//'`
+            format=`cat $TMPFILE | sed -n "/$audio/,/Forced/p" | grep "^Format  " | cut -d ":" -f2 | sed -e 's/ //' -e 's/UTF\-8//'`
             if [ "$format" ]; then echo -n " | $format" ; fi
-            channels=`cat $TMP/mediainfo.txt | sed -n "/$audio/,/Forced/p" | grep "^Channel(s)" | cut -d ":" -f2 | sed 's/ //'`
+            channels=`cat $TMPFILE | sed -n "/$audio/,/Forced/p" | grep "^Channel(s)" | cut -d ":" -f2 | sed 's/ //'`
             if [ "$channels" ]; then echo -n " $channels" ; fi
-            language=`cat $TMP/mediainfo.txt | sed -n "/$audio/,/Forced/p" | grep "^Language  " | cut -d ":" -f2 | sed 's/ //'`
+            language=`cat $TMPFILE | sed -n "/$audio/,/Forced/p" | grep "^Language  " | cut -d ":" -f2 | sed 's/ //'`
             if [ "$language" ]; then echo -n " $language" ; fi
             echo
 	done
+	rm $TMPFILE
     done
 fi
 
